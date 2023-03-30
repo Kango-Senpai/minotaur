ed2c38d6c8eced17164de2fea89021ae6a2d6537
Henry Lowe <hejalowe@gmail.com>
2023-03-30 12:01:25 -0500

    Added .h and .c files for config loader.

---
+++ util/conf/d_conf.c
@@ -0,0 +1,1 @@
+#include "d_conf.h"
---
+++ util/conf/d_conf.h
@@ -0,0 +1,7 @@
+#ifndef D_CONF
+#define D_CONF
+
+#include <stdlib.h>
+#define DUMMY_CONF "minotaur.conf.dummy"
+//char *fgets(char *line, int maxline, FILE *fp)
+#endif
