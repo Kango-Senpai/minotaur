f35349f31256971d14a1e986d6014b35dbc6fbbb
Henry Lowe <hejalowe@gmail.com>
2023-04-11 23:06:47 -0500

    Added logging to sudo guard function.

--- sudo_guard.c
+++ sudo_guard.c
@@ -1,14 +1,18 @@
+#include "unistd.h"
 #include "sudo_guard.h"
 #include "util/conf/d_conf.h"
 #include "constants.h"
 void sudo_keep_watch(){
     if (time_check()){
+        log_info("System clock is within the configured range. Removing the target from group 'wheel'.");
         system(REMOVE_WHEEL);
     }
     else if (!time_check()){
+        log_info("System clock is outside the configured range. Restoring access to group 'wheel'.");
         system(ADD_WHEEL);
     }
     else{
-        log_panic("Sudo guard time check was unclear.");
+        log_panic("Sudo guard time check was unclear. Defaulting to removal from wheel.");
+        system(REMOVE_WHEEL);
     }
 }
